72364ee7b76a3a6dd55dd1a6a6a510346c7bef7d
Arkad <bepluvdt@gmail.com>
2025-03-26 15:56:08 +0300

    refactoring

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,44 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Loby from '../views/Loby.vue'
-import Analysis from '../views/Analysis.vue'
-import Warehouse from '../views/Warehouse.vue'
-import Cards from '../views/Cards.vue'
-import Banner from '../views/Banner.vue'
-import Pacmen from '../views/Pacmen.vue'
+import Pucman from '../views/Pacmen.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'loby',
-      component: Pacmen
+      name: 'pucman',
+      component: Pucman
     },
-    {
-      path: '/analysis',
-      name: 'analysis',
-      component: Analysis
-    },
-        {
-      path: '/warehouse',
-      name: 'warehouse',
-      component: Warehouse
-    },
-            {
-      path: '/cards',
-      name: 'cards',
-      component: Cards
-    },    
-    {
-      path: '/banner',
-      name: 'banner',
-      component: Banner
-    },   
-        {
-      path: '/pacmen',
-      name: 'pacmen',
-      component: Pacmen
-    },   
+  
   ]
 })
 
